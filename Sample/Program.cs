e20cb047b1b03b314a6bb3b89224ae3bd5638259
David Fowler <davidfowl@gmail.com>
2021-05-04 20:24:21 -0700

    Force database creation

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDbContext2>(o => o.UseSqlite("DataSource=Todos.db"));
 var app = builder.Build();
 
+// This makes sure the database and tables are created
+var db = new TodoDbContext();
+db.Database.EnsureCreated();
+
 // TodoApi.MapRoutes(app);
 // TodoApi2.MapRoutes(app);
 // TodoApi3.MapRoutes(app);
