61310854ecb65e839821a339f5cb313daf70708f
David Fowler <davidfowl@gmail.com>
2021-06-18 20:56:59 -0700

    Added open api

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Todos") ?? "Data Source=Todos.db";
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<TodoDbContext>(o => o.UseSqlite(connectionString));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{builder.Environment.ApplicationName} v1"));
+}
 
 TodoApi2.MapRoutes(app);
 
