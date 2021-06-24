6174cd544311bd2287890d6d3e322556d9674a8f
David Fowler <davidfowl@gmail.com>
2021-06-24 11:48:39 -0700

    Removed CreatedAt

--- Sample/Results.cs
+++ Sample/Results.cs
@@ -8,7 +8,6 @@ namespace Microsoft.AspNetCore.Http
         public static IResult Ok() => new StatusCodeResult(200);
         public static IResult Status(int statusCode) => new StatusCodeResult(statusCode);
         public static IResult Ok(object value) => new JsonResult(value);
-        public static IResult CreatedAt(object value, string endpointName, object values) => new CreatedAtRouteResult(value, endpointName, values);
         public static IResult Created(string location, object value) => new CreatedResult(location, value);
 
         private class CreatedResult : IResult
@@ -26,30 +25,6 @@ namespace Microsoft.AspNetCore.Http
             {
                 httpContext.Response.StatusCode = StatusCodes.Status201Created;
                 httpContext.Response.Headers.Location = _location;
-
-                return httpContext.Response.WriteAsJsonAsync(_value);
-            }
-        }
-
-        private class CreatedAtRouteResult : IResult
-        {
-            private readonly object _value;
-            private readonly string _endpointName;
-            private readonly object _values;
-
-            public CreatedAtRouteResult(object value, string endpointName, object values)
-            {
-                _value = value;
-                _endpointName = endpointName;
-                _values = values;
-            }
-
-            public Task ExecuteAsync(HttpContext httpContext)
-            {
-                var linkGenerator = httpContext.RequestServices.GetRequiredService<LinkGenerator>();
-
-                httpContext.Response.StatusCode = StatusCodes.Status201Created;
-                httpContext.Response.Headers.Location = linkGenerator.GetPathByName(_endpointName, _values);
 
                 return httpContext.Response.WriteAsJsonAsync(_value);
             }
