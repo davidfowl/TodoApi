82898c59a8215ad49c387d1d793ef6c3b50bef67
David Fowler <davidfowl@gmail.com>
2022-12-02 21:10:35 -0800

    Add support for social authentication (#45)
    * Added social login
    - Added support for social providers
    
    * Use nameof
    
    * Clean up auth redirect code
    - Use a redirect and temporary cookie instead of trying to handle it in the oauth handler events.
    
    * Added a service to resolve social providers
    
    * Added a CurrentUserName property to the Model
    
    * Added test for new token API
    
    * Add typed delegate to clean up the generics
    
    * Added support for Microsoft account
    
    * Same site cookies don't work here :|
    
    * Added more comments a readme section for setup
    
    * Removed comment.

--- Todo.Web/Server/Pages/_Host.cshtml.cs
+++ Todo.Web/Server/Pages/_Host.cshtml.cs
@@ -4,7 +4,19 @@ namespace Todo.Web.Server.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    private readonly SocialProviders _socialProviders;
+
+    public IndexModel(SocialProviders socialProviders)
     {
+        _socialProviders = socialProviders;
+    }
+
+    public string[] ProviderNames { get; set; } = default!;
+    public string? CurrentUserName { get; set; }
+
+    public async Task OnGet()
+    {
+        ProviderNames = await _socialProviders.GetProviderNamesAsync();
+        CurrentUserName = User.Identity!.Name;
     }
 }
--- TodoApi/Users/UsersApi.cs
+++ TodoApi/Users/UsersApi.cs
@@ -29,12 +29,38 @@ public static class UsersApi
         {
             var user = await userManager.FindByNameAsync(userInfo.Username);
 
-            if (user == null || !await userManager.CheckPasswordAsync(user, userInfo.Password))
+            if (user is null || !await userManager.CheckPasswordAsync(user, userInfo.Password))
             {
                 return TypedResults.BadRequest();
             }
 
             return TypedResults.Ok(new AuthToken(tokenService.GenerateToken(user.UserName!)));
+        });
+
+        group.MapPost("/token/{provider}", async Task<Results<Ok<AuthToken>, ValidationProblem>> (string provider, ExternalUserInfo userInfo, UserManager<TodoUser> userManager, ITokenService tokenService) =>
+        {
+            var user = await userManager.FindByLoginAsync(provider, userInfo.ProviderKey);
+
+            var result = IdentityResult.Success;
+
+            if (user is null)
+            {
+                user = new TodoUser() { UserName = userInfo.Username };
+
+                result = await userManager.CreateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    result = await userManager.AddLoginAsync(user, new UserLoginInfo(provider, userInfo.ProviderKey, displayName: null));
+                }
+            }
+
+            if (result.Succeeded)
+            {
+                return TypedResults.Ok(new AuthToken(tokenService.GenerateToken(user.UserName!)));
+            }
+
+            return TypedResults.ValidationProblem(result.Errors.ToDictionary(e => e.Code, e => new[] { e.Description }));
         });
 
         return group;
