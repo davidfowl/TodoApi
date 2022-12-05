db01ed9103708404cdf68eee655f86113c498d4d
mehdihadeli <mehdi.hadeli@outlook.com>
2022-12-05 15:49:48 +0330

    some serilog refactoring

--- TodoApi/Extensions/SerilogExtensions.cs
+++ TodoApi/Extensions/SerilogExtensions.cs
@@ -11,8 +11,7 @@ public static class SerilogExtensions
         string sectionName = "Serilog")
     {
         builder.Services.AddOptions<SerilogOptions>()
-            .BindConfiguration(sectionName)
-            .ValidateDataAnnotations();
+            .BindConfiguration(sectionName);
 
         builder.Host.UseSerilog((context, serviceProvider, loggerConfiguration) =>
         {
@@ -24,8 +23,6 @@ public static class SerilogExtensions
             loggerConfiguration
                 .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
                 .Enrich.FromLogContext()
-                .Enrich.WithEnvironmentName()
-                .Enrich.WithMachineName()
                 // https://rehansaeed.com/logging-with-serilog-exceptions/
                 .Enrich.WithExceptionDetails();
 
@@ -39,31 +36,29 @@ public static class SerilogExtensions
             if (!string.IsNullOrEmpty(loggerOptions.ElasticSearchUrl))
             {
                 // https://github.com/serilog-contrib/serilog-sinks-elasticsearch
-                loggerConfiguration.WriteTo.Async(
-                    writeTo =>
-                        writeTo.Elasticsearch(new(new Uri(loggerOptions.ElasticSearchUrl))
-                        {
-                            AutoRegisterTemplate = true,
-                            IndexFormat = builder.Environment.ApplicationName
-                        }));
+                loggerConfiguration.WriteTo.Elasticsearch(new(new Uri(loggerOptions.ElasticSearchUrl))
+                {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = builder.Environment.ApplicationName
+                });
             }
 
             if (!string.IsNullOrEmpty(loggerOptions.SeqUrl))
             {
-                loggerConfiguration.WriteTo.Async(writeTo => writeTo.Seq(loggerOptions.SeqUrl));
+                loggerConfiguration.WriteTo.Seq(loggerOptions.SeqUrl);
             }
         });
 
         return builder;
     }
-    
+
     private sealed class SerilogOptions
     {
         public bool UseConsole { get; set; } = true;
         public string? SeqUrl { get; set; } = default!;
         public string? ElasticSearchUrl { get; set; } = default!;
+
         public string LogTemplate { get; set; } =
             "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level} - {Message:lj}{NewLine}{Exception}";
     }
 }
-
