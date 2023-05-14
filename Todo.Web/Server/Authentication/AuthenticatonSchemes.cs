6090f8dfc7ecae0ec4539b5d42fa2aa1ee270246
David Fowler <davidfowl@gmail.com>
2023-05-13 21:54:26 -0700

    Readonly

--- Todo.Web/Server/Authentication/AuthenticatonSchemes.cs
+++ Todo.Web/Server/Authentication/AuthenticatonSchemes.cs
@@ -3,5 +3,5 @@
 public class AuthenticatonSchemes
 {
     // This is the scheme used to store login information from external providers
-    public static string ExternalScheme { get; } = "External";
+    public static string ExternalScheme => "External";
 }
