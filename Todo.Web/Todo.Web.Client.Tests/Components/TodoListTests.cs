be2f450006252e576cd294bd42def1a136e00d42
johnny <johnny.zou@azenix.com.au>
2023-08-01 15:20:43 +1000

    Template json.

--- Todo.Web/Todo.Web.Client.Tests/Components/TodoListTests.cs
+++ Todo.Web/Todo.Web.Client.Tests/Components/TodoListTests.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using AutoFixture;
 using Bunit;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,17 +12,43 @@ namespace Todo.Web.Client.Tests.Components;
 
 public sealed class TodoListTests : TestContext
 {
+    private readonly IFixture _fixture = new Fixture();
+
     [Fact]
     public async Task RendersTodoItems()
     {
-        var getResponse = this.WithSnapshot();
+        var todos = _fixture.Build<GetTodos_Todos_Todo>().CreateMany().ToArray();
+
+        List<JsonNode> nodes = new();
+        foreach (var todo in todos)
+        {
+            var template = $"{{ " +
+                $"\"__typename\": \"Todo\",  " +
+                $"\"id\": {todo.Id}, " +
+                $"\"isComplete\": {todo.IsComplete.ToString().ToLowerInvariant()}, " +
+                $"\"title\": \"{todo.Title}\" " +
+                "}";
+
+            var node = JsonNode.Parse(template)!;
+            nodes.Add(node);
+        }
+        var array = new JsonArray(nodes.ToArray());
+        var document = JsonDocument.Parse($"{{\"data\": {{ \"todos\": {array} }} }}");
+        var paylaod = document.RootElement.GetRawText();
 
         Services
             .AddTodoClient()
             .ConfigureHttpClient(
                 client => client.BaseAddress = new Uri("http://test/graphql"),
                 builder => builder.ConfigurePrimaryHttpMessageHandler(
-                    ProxyHttpMessageHandler.Create(async _ => await getResponse())));
+                    ProxyHttpMessageHandler.Create(request =>
+                    {
+                        return Task.FromResult<HttpResponseMessage?>(
+                            new HttpResponseMessage(HttpStatusCode.OK)
+                            {
+                                Content = new StringContent(paylaod)
+                            });
+                    })));
 
         var page = RenderComponent<TodoList>();
 
--- TodoApi/GraphQL/Types/Query.cs
+++ TodoApi/GraphQL/Types/Query.cs
@@ -1,4 +1,3 @@
-using HotChocolate.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace TodoApi.GraphQL.Types;
